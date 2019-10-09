ca42517e22a937aba0a396211849a9634e1935f8
Maxime Barbot <maxime.b@sportheroes.com>
2019-10-09 19:18:33 +0200

    [ADD] Setup project

---
+++ src/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  upload: {
+    maxSize: 3000000,
+    allowedExtensions: [
+      "image/png",
+      "image/jpeg",
+      "image/jpg",
+      "application/pdf"
+    ]
+  }
+};
---
+++ src/utils/color.js
@@ -0,0 +1,20 @@
+export const hexToRgb = hex => {
+  // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+  const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+  hex = hex.replace(shorthandRegex, (m, r, g, b) => r + r + g + g + b + b);
+
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result
+    ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16),
+        a: 1,
+      }
+    : null;
+};
+
+export const rgb2hex = (red, green, blue) => {
+  const rgb = blue | (green << 8) | (red << 16);
+  return `#${(0x1000000 + rgb).toString(16).slice(1)}`;
+};
